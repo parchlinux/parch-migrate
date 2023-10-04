c77e1063c9526dac77bb68251cffea4bef6bdd39
Sohrab Behdani <behdanisohrab@gmail.com>
2023-10-04 21:30:50 +0330

    initial release of the script

---
+++ migrate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+read -p "This script would turn your current distro to Parch Linux and after running it there is no going back, Are you sure? (y/n): " choice
+
+if [[ $choice == "y" ]]; then
+    echo "Starting the migration...."
+    cd /tmp
+    wget https://github.com/parchlinux/ppr/raw/main/x86_64/parch-branding-2023.11.19-1-any.pkg.tar.zst
+    pacman -U parch-branding-2023.11.19-1-any.pkg.tar.zst
+    wget https://github.com/parchlinux/ppr/raw/main/x86_64/parch-pacman-24-1-any.pkg.tar.zst
+    pacman -U parch-pacman-24-1-any.pkg.tar.zst
+    pacman -Syy
+    echo "Done, please reboot your system."
+else
+    echo "Exiting..."
+    exit 0
+fi
