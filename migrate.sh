93d87001955ca3ebc1a81ca386c94c1ebdcd3b63
Sohrab Behdani <behdanisohrab@gmail.com>
2023-11-14 10:31:59 +0330

    fixed some issues, added support for WSL

--- migrate.sh
+++ migrate.sh
@@ -6,11 +6,12 @@ if [[ $choice == "y" ]]; then
     echo "Starting the migration...."
     cd /tmp
     wget https://github.com/parchlinux/ppr/raw/main/x86_64/parch-branding-2023.11.19-1-any.pkg.tar.zst
-    pacman -U parch-branding-2023.11.19-1-any.pkg.tar.zst
+    pacman -U parch-branding-2023.11.19-1-any.pkg.tar.zst --noconfirm
     wget https://github.com/parchlinux/ppr/raw/main/x86_64/parch-pacman-24-1-any.pkg.tar.zst
-    pacman -U parch-pacman-24-1-any.pkg.tar.zst
+    pacman -U parch-pacman-24-1-any.pkg.tar.zst --noconfirm
     pacman -Syy
-    pacman -S parch-emoji-ios parch-wallpapers
+    pacman -Rdd neofetch --noconfirm
+    pacman -S parch-emoji-ios archlinux-keyring neofetch-git parch-wallpapers --noconfirm
     echo "Done, please reboot your system."
 else
     echo "Exiting..."
