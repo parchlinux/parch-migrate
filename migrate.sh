f484dc72c4b296f4b6c48591d0317c2e05e50f0a
Sohrab Behdani <72007820+behdanisohrab@users.noreply.github.com>
2024-02-18 09:13:53 +0330

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -5,10 +5,10 @@ read -p "This script would turn your current distro to Parch Linux and after run
 if [[ $choice == "y" ]]; then
     echo "Starting the migration...."
     cd /tmp
-    wget https://github.com/parchlinux/ppr/raw/main/x86_64/parch-branding-2023.11.19-1-any.pkg.tar.zst
-    pacman -U parch-branding-2023.11.19-1-any.pkg.tar.zst --noconfirm
-    wget https://github.com/parchlinux/ppr/raw/main/x86_64/parch-pacman-24-1-any.pkg.tar.zst
-    pacman -U parch-pacman-24-1-any.pkg.tar.zst --noconfirm
+    wget https://raw.githubusercontent.com/parchlinux/ppr/main/x86_64/parch-branding-2023.11.22-0-any.pkg.tar.zst
+    pacman -U parch-branding-2023.11.22-0-any.pkg.tar.zst --noconfirm
+    wget https://raw.githubusercontent.com/parchlinux/ppr/main/x86_64/parch-pacman-26-2-any.pkg.tar.zst
+    pacman -U parch-pacman-26-2-any.pkg.tar.zst --noconfirm
     pacman -Syy
     pacman -Rdd neofetch --noconfirm
     pacman -S parch-emoji-ios archlinux-keyring neofetch-git parch-wallpapers --noconfirm
